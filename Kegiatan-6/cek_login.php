d574324fd2dd357566a48fc0c4805245a7bdc4da
xscots <rizqi_rosalin@yahoo.co.id>
2021-11-23 21:00:30 +0800

    Rizqi-Praimadi-Rosalin_1900018198

---
+++ Kegiatan-6/cek_login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include "koneksi.php";
+$id_user = $_POST['id_user'];
+$pass=($_POST['password']);
+$sql="SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'";
+if ($_POST["captcha_code"] == $_SESSION["captcha_code"]) {
+ 
+$login=mysqli_query($con,$sql);
+$ketemu=mysqli_num_rows($login);
+$r= mysqli_fetch_array($login);
+if ($ketemu > 0){
+ $_SESSION['id_user'] = $r['id_user'];
+ $_SESSION['password_user'] = $r['password'];
+echo"USER BERHASIL LOGIN<br>";
+echo "id user =",$_SESSION['id_user'],"<br>";
+echo "password=",$_SESSION['password_user'],"<br>";
+echo "<a href=logout.php><b>LOGOUT</b></a></center>";
+}
+else{
+ echo "<center>Login gagal! username & password tidak benar<br>";
+ echo "<a href=form_login.php><b>ULANGI LAGI</b></a></center>";
+}
+mysqli_close($con); 
+} 
+else {
+echo "<center>Login gagal! Captcha tidak sesuai<br>";
+ echo "<a href=form_login.php><b>ULANGI LAGI</b></a></center>"; }
---
+++ Kegiatan-6/logout.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+session_destroy();
+echo "Anda telah sukses keluar sistem <b>LOGOUT</b>";
